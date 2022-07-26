919b856015f0d37948a129771b998a4b57a39897
TeBeCo (wsl) <TeBeCo@gmail.com>
2022-07-26 16:44:53 +0400

    use `remote-deploy` on control plane

--- bash-scripts/deploy.sh
+++ bash-scripts/deploy.sh
@@ -9,9 +9,14 @@
 # * sed s/KEY_PLACEHOLDER/$(cat -n <keyfilehere>) ./cloudinit-template.yml | tee ./cloudinit.yml
 chmod 400 ~/.ssh/kube-key-ecdsa
 
-
 cd ./infra || exit
 ./deploy.sh
 
-cd ./controlplane || exit
-./deploy.sh
+
+
+SOURCE_FILES="./controlplane/files.txt"
+TARGET_USER="tebeco"
+TARGET_HOST="192.168.104.10"
+TARGET_FOLDER="/home/tebeco/instal-cp"
+
+./common/remote-deploy.sh "$(readlink -f "$SOURCE_FILES")" "$TARGET_USER" "$TARGET_HOST" "$TARGET_FOLDER"
